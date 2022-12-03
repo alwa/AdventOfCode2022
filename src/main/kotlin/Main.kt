083e6b8f0ba3096b5315a41963688ff09ce31e83
alixwar <alix.warnke@assaabloy.com>
2022-12-03 11:53:59 +0100

    Fixed mistakes in Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,9 @@ fun main() {
     println("=Day 1=")
     println("Part 1: ${Day1.part1("day1/input.txt")}")
     println("Part 2: ${Day1.part2("day1/input.txt")}")
-    println("Part 2: ${Day2.part1("day2/input.txt")}")
+    println("=Day 2=")
+    println("Part 1: ${Day2.part1("day2/input.txt")}")
     println("Part 2: ${Day2.part2("day2/input.txt")}")
-    println("Part 3: ${Day3.part1("day3/input.txt")}")
+    println("=Day 2=")
+    println("Part 1: ${Day3.part1("day3/input.txt")}")
 }
