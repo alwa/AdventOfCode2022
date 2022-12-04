8b7c327de2c0c87e320725edaa45aadae5e3a5b1
alixwar <alix.warnke@assaabloy.com>
2022-12-04 09:49:56 +0100

    Fixed typo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ fun main() {
     println("=Day 2=")
     println("Part 1: ${Day2.part1("day2/input.txt")}")
     println("Part 2: ${Day2.part2("day2/input.txt")}")
-    println("=Day 2=")
+    println("=Day 3=")
     println("Part 1: ${Day3.part1("day3/input.txt")}")
     println("Part 2: ${Day3.part2("day3/input.txt")}")
 }
