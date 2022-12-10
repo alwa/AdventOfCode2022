fbf00cda5c0f72ee5dfe48c2a0ad665798173a50
alixwar <alix.warnke@assaabloy.com>
2022-12-11 00:02:30 +0100

    Day 9

--- src/main/kotlin/org/example/challenges/Day9.kt
+++ src/main/kotlin/org/example/challenges/Day9.kt
@@ -28,21 +28,7 @@ object Day9 : TwoPartChallenge<Int> {
                 } else {
                     throw IllegalStateException()
                 }
-                if (head.x == tail.x && abs(head.y - tail.y) > 1) {
-                    // Y move
-                    val newY = tail.y + (head.y - tail.y) / abs(head.y - tail.y)
-                    tail = Coordinate(x = tail.x, y = newY)
-                } else if (head.y == tail.y && abs(head.x - tail.x) > 1) {
-                    // X move
-                    val newX = tail.x + (head.x - tail.x) / abs(head.x - tail.x)
-                    tail = Coordinate(x = newX, y = tail.y)
-                } else if (head.x != tail.x && head.y != tail.y && abs(head.y - tail.y) > 1 ||
-                    head.x != tail.x && head.y != tail.y && abs(head.x - tail.x) > 1 ) {
-                    // Diagonal move
-                    val newX = tail.x + (head.x - tail.x) / abs(head.x - tail.x)
-                    val newY = tail.y + (head.y - tail.y) / abs(head.y - tail.y)
-                    tail = Coordinate(x = newX, y = newY)
-                }
+                tail = followHead(head, tail)
                 positionsHeadHasVisited.add(head)
                 positionsTailHasVisited.add(tail)
             }
@@ -50,8 +36,71 @@ object Day9 : TwoPartChallenge<Int> {
         return positionsTailHasVisited.size
     }
 
+    private fun followHead(
+        head: Coordinate,
+        tail: Coordinate
+    ): Coordinate {
+        var tail1 = tail
+        if (head.x == tail1.x && abs(head.y - tail1.y) > 1) {
+            // Y move
+            val newY = tail1.y + (head.y - tail1.y) / abs(head.y - tail1.y)
+            tail1 = Coordinate(x = tail1.x, y = newY)
+        } else if (head.y == tail1.y && abs(head.x - tail1.x) > 1) {
+            // X move
+            val newX = tail1.x + (head.x - tail1.x) / abs(head.x - tail1.x)
+            tail1 = Coordinate(x = newX, y = tail1.y)
+        } else if (head.x != tail1.x && head.y != tail1.y && abs(head.y - tail1.y) > 1 ||
+            head.x != tail1.x && head.y != tail1.y && abs(head.x - tail1.x) > 1
+        ) {
+            // Diagonal move
+            val newX = tail1.x + (head.x - tail1.x) / abs(head.x - tail1.x)
+            val newY = tail1.y + (head.y - tail1.y) / abs(head.y - tail1.y)
+            tail1 = Coordinate(x = newX, y = newY)
+        }
+        return tail1
+    }
+
     override fun part2(file: File): Int {
-        TODO("Not yet implemented")
+        var head = Coordinate(x = 0, y = 0)
+        val tail = mutableListOf(
+            Coordinate(x = 0, y = 0),
+            Coordinate(x = 0, y = 0),
+            Coordinate(x = 0, y = 0),
+            Coordinate(x = 0, y = 0),
+            Coordinate(x = 0, y = 0),
+            Coordinate(x = 0, y = 0),
+            Coordinate(x = 0, y = 0),
+            Coordinate(x = 0, y = 0),
+            Coordinate(x = 0, y = 0)
+        )
+        val positionsTailHasVisited = mutableSetOf<Coordinate>()
+        val positionsHeadHasVisited = mutableSetOf<Coordinate>()
+        file.forEachLine { line ->
+            val parts = line.split(" ")
+            val steps = parts[1].toInt()
+            for (i in 0 until steps) {
+                positionsHeadHasVisited.add(head)
+                positionsTailHasVisited.add(tail[8])
+                head = if (parts[0] == "R") {
+                    Coordinate(head.x + 1, head.y)
+                } else if (parts[0] == "L") {
+                    Coordinate(head.x - 1, head.y)
+                } else if (parts[0] == "U") {
+                    Coordinate(head.x, head.y + 1)
+                } else if (parts[0] == "D") {
+                    Coordinate(head.x, head.y - 1)
+                } else {
+                    throw IllegalStateException()
+                }
+                tail[0] = followHead(head, tail[0])
+                for (j in 0  until 8) {
+                    tail[j+1] = followHead(head = tail[j], tail = tail[j+1])
+                }
+                positionsHeadHasVisited.add(head)
+                positionsTailHasVisited.add(tail[8])
+            }
+        }
+        return positionsTailHasVisited.size
     }
 
     private data class Coordinate(val x: Int, val y: Int)
--- src/test/kotlin/org/example/challenges/Day9Test.kt
+++ src/test/kotlin/org/example/challenges/Day9Test.kt
@@ -11,4 +11,10 @@ class Day9Test {
         assertEquals(13, Day9.part1(File(ClassLoader.getSystemResource("day9/testinput.txt").file)))
     }
 
+    @Test
+    fun `Part 2`() {
+        assertEquals(1, Day9.part2(File(ClassLoader.getSystemResource("day9/testinput.txt").file)))
+        assertEquals(36, Day9.part2(File(ClassLoader.getSystemResource("day9/testinput2.txt").file)))
+    }
+
 }
