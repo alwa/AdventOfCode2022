bf494b5f1e5cb5cc6a7709fa5dd4292cc0994e32
alixwar <alix.warnke@assaabloy.com>
2022-12-11 00:55:30 +0100

    Day 10 part 1

--- src/main/kotlin/org/example/challenges/Day10.kt
+++ src/main/kotlin/org/example/challenges/Day10.kt
@@ -6,7 +6,25 @@ import java.io.File
 object Day10 : TwoPartChallenge<Int> {
 
     override fun part1(file: File): Int {
-        TODO("Not yet implemented")
+        var sum = 0
+        val cycles = mutableListOf(1)
+        val indicesToCount = setOf(20, 60, 100, 140, 180, 220)
+        file.forEachLine { line ->
+            if (line.startsWith("noop")) {
+                cycles.add(0)
+            } else {
+                val parts = line.split(" ")
+                check(parts[0] == "addx")
+                cycles.add(0)
+                cycles.add(parts[1].toInt())
+            }
+        }
+        cycles.forEachIndexed { index, i ->
+            if ((index+1) in indicesToCount) {
+                sum += (index+1) * cycles.subList(0, index+1).sum()
+            }
+        }
+        return sum
     }
 
     override fun part2(file: File): Int {
