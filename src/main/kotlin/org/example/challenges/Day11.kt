be5e35bb3f9f039f66cc4b4dd018c5bee4134ee9
alixwar <alix.warnke@assaabloy.com>
2022-12-13 23:37:18 +0100

    Day 11 - Part 1

--- src/main/kotlin/org/example/challenges/Day11.kt
+++ src/main/kotlin/org/example/challenges/Day11.kt
@@ -8,16 +8,16 @@ object Day11 : TwoPartChallenge<Int, Int> {
 
     override fun part1(file: File): Int {
         val monkeys = mutableListOf<Monkey>()
-        var startingItems: Stack<Int> = Stack()
-        var operation: (Int) -> Int = { it }
-        var test = 0
+        var startingItems: Stack<Long> = Stack()
+        var operation: (Long) -> Long = { it }
+        var test = 0L
         var ifTrueMonkey = 0
         var ifFalseMonkey: Int
         file.forEachLine { line ->
             if (line.trimStart().startsWith("Starting items:")) {
                 val startingItemsRaw = line.substring(line.indexOf(":") + 1).trimStart().split(",").asReversed()
                 for (startingItem in startingItemsRaw) {
-                    startingItems.push(startingItem.trimStart().trimEnd().toInt())
+                    startingItems.push(startingItem.trimStart().trimEnd().toLong())
                 }
             }
             if (line.trimStart().startsWith("Operation:")) {
@@ -25,23 +25,23 @@ object Day11 : TwoPartChallenge<Int, Int> {
                 if (operationRaw.contains("*")) {
                     val secondOperator = operationRaw.substring(operationRaw.indexOf("*") + 1).trimStart().trimEnd()
                     operation = if (secondOperator == "old") {
-                        { i: Int -> i * i }
+                        { i: Long -> i * i }
                     } else {
-                        { i: Int -> i * secondOperator.toInt() }
+                        { i: Long -> i * secondOperator.toLong() }
                     }
                 } else if (operationRaw.contains("+")) {
                     val secondOperator = operationRaw.substring(operationRaw.indexOf("+") + 1).trimStart().trimEnd()
                     operation = if (secondOperator == "old") {
-                        { i: Int -> i + i }
+                        { i: Long -> i + i }
                     } else {
-                        { i: Int -> i + secondOperator.toInt() }
+                        { i: Long -> i + secondOperator.toLong() }
                     }
                 } else {
                     throw IllegalStateException()
                 }
             }
             if (line.trimStart().startsWith("Test: divisible by ")) {
-                test = line.substring(line.indexOf("by") + 3).trimStart().trimEnd().toInt()
+                test = line.substring(line.indexOf("by") + 3).trimStart().trimEnd().toLong()
             }
             if (line.trimStart().startsWith("If true: throw to monkey ")) {
                 ifTrueMonkey = line.substring(line.length - 1).toInt()
@@ -57,20 +57,20 @@ object Day11 : TwoPartChallenge<Int, Int> {
                         startingItems = startingItems
                     )
                 )
-                startingItems = Stack<Int>()
+                startingItems = Stack<Long>()
             }
         }
-        for (i: Int in 0 until (20)) {
+        for (i: Int in 0 until 20) {
             monkeys.forEachIndexed { index, monkey ->
-                while (monkey.startingItems.isNotEmpty()) {
-                    val inspectedItem: Int = monkey.startingItems.pop()
+                while (monkeys[index].startingItems.isNotEmpty()) {
+                    val inspectedItem: Long = monkey.startingItems.pop()
                     monkey.numberOfInspections++
-                    val worryAfterInspection: Int = monkey.operation.invoke(inspectedItem)
-                    val worryAfterBored: Int = worryAfterInspection / 3
-                    if (worryAfterBored % monkey.test == 0) {
-                        monkeys[monkey.ifTrue].startingItems.push( worryAfterBored)
+                    val worryAfterInspection: Long = monkey.operation.invoke(inspectedItem)
+                    val worryAfterBored: Long = worryAfterInspection / 3
+                    if (worryAfterBored % monkey.test == 0L) {
+                        monkeys[monkey.ifTrue].startingItems.add(0, worryAfterBored)
                     } else {
-                        monkeys[monkey.ifFalse].startingItems.add( worryAfterBored)
+                        monkeys[monkey.ifFalse].startingItems.add( 0,worryAfterBored)
                     }
                 }
             }
@@ -93,8 +93,8 @@ object Day11 : TwoPartChallenge<Int, Int> {
     }
 
     private data class Monkey(
-        val test: Int, val ifTrue: Int, val ifFalse: Int, val operation: (Int) -> Int,
-        val startingItems: Stack<Int>, var numberOfInspections: Int = 0
+        val test: Long, val ifTrue: Int, val ifFalse: Int, val operation: (Long) -> Long,
+        val startingItems: Stack<Long>, var numberOfInspections: Int = 0
     )
 
 }
