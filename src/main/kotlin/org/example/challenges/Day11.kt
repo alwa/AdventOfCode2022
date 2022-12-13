6b16be94417c55639ec1ed92acc33133c641eca9
alixwar <alix.warnke@assaabloy.com>
2022-12-13 23:42:54 +0100

    Refactoring

--- src/main/kotlin/org/example/challenges/Day11.kt
+++ src/main/kotlin/org/example/challenges/Day11.kt
@@ -7,12 +7,33 @@ import java.util.*
 object Day11 : TwoPartChallenge<Int, Int> {
 
     override fun part1(file: File): Int {
+        val monkeys = setupMonkeys(file)
+        for (i: Int in 0 until 20) {
+            monkeys.forEachIndexed { index, monkey ->
+                while (monkeys[index].startingItems.isNotEmpty()) {
+                    val inspectedItem: Long = monkey.startingItems.pop()
+                    monkey.numberOfInspections++
+                    val worryAfterInspection: Long = monkey.operation.invoke(inspectedItem)
+                    val worryAfterBored: Long = worryAfterInspection / 3
+                    if (worryAfterBored % monkey.test == 0L) {
+                        monkeys[monkey.ifTrue].startingItems.add(0, worryAfterBored)
+                    } else {
+                        monkeys[monkey.ifFalse].startingItems.add(0, worryAfterBored)
+                    }
+                }
+            }
+        }
+        val sortedInspections: SortedSet<Int> = sortedSetOf()
+        monkeys.forEach { monkey -> sortedInspections.add(monkey.numberOfInspections) }
+        return sortedInspections.toList().subList(sortedInspections.size - 2, sortedInspections.size).product()
+    }
+
+    private fun setupMonkeys(file: File): List<Monkey> {
         val monkeys = mutableListOf<Monkey>()
         var startingItems: Stack<Long> = Stack()
         var operation: (Long) -> Long = { it }
         var test = 0L
         var ifTrueMonkey = 0
-        var ifFalseMonkey: Int
         file.forEachLine { line ->
             if (line.trimStart().startsWith("Starting items:")) {
                 val startingItemsRaw = line.substring(line.indexOf(":") + 1).trimStart().split(",").asReversed()
@@ -47,12 +68,11 @@ object Day11 : TwoPartChallenge<Int, Int> {
                 ifTrueMonkey = line.substring(line.length - 1).toInt()
             }
             if (line.trimStart().startsWith("If false: throw to monkey ")) {
-                ifFalseMonkey = line.substring(line.length - 1).toInt()
                 monkeys.add(
                     Monkey(
                         test = test,
                         ifTrue = ifTrueMonkey,
-                        ifFalse = ifFalseMonkey,
+                        ifFalse = line.substring(line.length - 1).toInt(),
                         operation = operation,
                         startingItems = startingItems
                     )
@@ -60,24 +80,7 @@ object Day11 : TwoPartChallenge<Int, Int> {
                 startingItems = Stack<Long>()
             }
         }
-        for (i: Int in 0 until 20) {
-            monkeys.forEachIndexed { index, monkey ->
-                while (monkeys[index].startingItems.isNotEmpty()) {
-                    val inspectedItem: Long = monkey.startingItems.pop()
-                    monkey.numberOfInspections++
-                    val worryAfterInspection: Long = monkey.operation.invoke(inspectedItem)
-                    val worryAfterBored: Long = worryAfterInspection / 3
-                    if (worryAfterBored % monkey.test == 0L) {
-                        monkeys[monkey.ifTrue].startingItems.add(0, worryAfterBored)
-                    } else {
-                        monkeys[monkey.ifFalse].startingItems.add( 0,worryAfterBored)
-                    }
-                }
-            }
-        }
-        val sortedInspections: SortedSet<Int> = sortedSetOf()
-        monkeys.forEach { monkey -> sortedInspections.add(monkey.numberOfInspections) }
-        return sortedInspections.toList().subList(sortedInspections.size - 2, sortedInspections.size).product()
+        return monkeys
     }
 
     override fun part2(file: File): Int {
